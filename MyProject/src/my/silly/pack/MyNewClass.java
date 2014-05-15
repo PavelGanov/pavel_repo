bc260cb3166ccc35fee8529d40bd7be39f97d9a8
Pavel Ganov <pavelganov@abv.bg>
2014-05-15 17:13:35 +0300

    ergr

--- MyProject/src/my/silly/pack/MyNewClass.java
+++ MyProject/src/my/silly/pack/MyNewClass.java
@@ -13,7 +13,7 @@ public class MyNewClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("ALALALerfgsergALALAL");
+		System.out.println("ALALALerfgsergALasffgALAL");
 	}
 
 }
