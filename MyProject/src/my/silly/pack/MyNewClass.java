2bdf33ffed0a806cc67c2059aebe6759c2882337
Pavel Ganov <pavelganov@abv.bg>
2014-05-15 16:27:39 +0300

    committing like a boss

---
+++ MyProject/src/my/silly/pack/MyNewClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package my.silly.pack;
+
+/**
+ * @author pganov
+ *
+ */
+public class MyNewClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
